946d29c014b2dfda27e1c2e6a6259daebdddee67
photodev <30632817+photodev@users.noreply.github.com>
2017-08-02 11:40:56 +0100

    First stage of test

---
+++ CapGeminiTechTest/BasketItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapGeminiTechTest
+{
+    class BasketItem
+    {
+        public string ItemName { get; private set; }
+        public double ItemPrice { get; set; }
+        public string Description { get; private set; }
+
+        public BasketItem(string itemName)
+        {
+            this.ItemName = itemName;
+            this.ItemPrice = GetDefaultPrice();
+            this.Description = this.ItemName;
+        }
+
+        private double GetDefaultPrice()
+        {
+            double price = 0;
+
+            switch (this.ItemName)
+            {
+                case "Apple":
+                    price = 0.60;
+                    break;
+
+                case "Orange":
+                    price = 0.25;
+                    break;
+            }
+
+            return price;
+        }
+    }
+}
---
+++ CapGeminiTechTest/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapGeminiTechTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<BasketItem> ItemsInBasket;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupApp();
+        }
+
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ScanItemCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                // Scan the item
+                ScanItem(cboShopping.Text);
+                // Set the total of all items scanned
+                SetBasketTotal();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SetupApp()
+        {
+            ItemsInBasket = new ObservableCollection<BasketItem>();
+            lstBasket.DataContext = ItemsInBasket;
+            cboShopping.Items.Add("Apple");
+            cboShopping.Items.Add("Orange");
+            cboShopping.SelectedIndex = 0;
+            txtTotal.Text = "0.00";
+        }
+
+        private void ScanItem(string itemName)
+        {
+            BasketItem item = new BasketItem(itemName);
+            ItemsInBasket.Add(item);
+        }
+
+        private void SetBasketTotal()
+        {
+            //IEnumerable<double> prices = ItemsInBasket.Select(p => p.ItemPrice);
+            txtTotal.Text = ItemsInBasket.Select(p => p.ItemPrice).Sum().ToString("F");
+        }
+    }
+}
